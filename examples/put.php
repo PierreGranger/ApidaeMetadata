e04ee265c7c788c8441afe50e67508c58a818d26
Pierre Granger <pierre.granger@apidae-tourisme.com>
2021-09-27 11:50:05 +0200

    Adaptations pour ApidaeCode 0.6

--- examples/put.php
+++ examples/put.php
@@ -1,77 +1,86 @@
 <?php
 
-    ini_set('display_errors',1) ;
-    error_reporting(E_ALL) ;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-    require_once(realpath(dirname(__FILE__)).'/../vendor/autoload.php') ;
-    require_once(realpath(dirname(__FILE__)).'/../config.inc.php') ;
+require_once(realpath(dirname(__FILE__)) . '/../vendor/autoload.php');
+require_once(realpath(dirname(__FILE__)) . '/../config.inc.php');
 
-    $ApidaeMetadata = new \PierreGranger\ApidaeMetadata(array_merge($configApidaeMetadata,Array('debug'=>true,'timer'=>true))) ;
+$ApidaeMetadata = new \PierreGranger\ApidaeMetadata(array_merge($configApidaeMetadata, array('debug' => true, 'timer' => true)));
 
-    $ApidaeMetadata->start(__FILE__) ;
+$ApidaeMetadata->start(__FILE__);
 
-    $noeud = 'test-pg' ;
-    $offre = 4683815 ;
+$noeud = 'test-pg';
+$offre = 866995;
 
-    $mdCpt = 1 ;
-    function showMd() {
-        global $ApidaeMetadata, $mdCpt, $offre, $noeud ;
-        $ApidaeMetadata->start('get '.$mdCpt) ;
-        try { $metadonnee = $ApidaeMetadata->get($offre,$noeud) ; } catch ( \Exception $e ) { echo '<pre>'.print_r($e,true).'</pre>' ; }
-        echo '<pre style="background:#498248;color:white;">'.print_r($metadonnee,true).'</pre>' ;
-        $ApidaeMetadata->stop('get '.$mdCpt++) ;
+$mdCpt = 1;
+function showMd()
+{
+    global $ApidaeMetadata, $mdCpt, $offre, $noeud;
+    $ApidaeMetadata->start('get ' . $mdCpt);
+    try {
+        $metadonnee = $ApidaeMetadata->get($offre, $noeud);
+    } catch (\Exception $e) {
+        echo '<pre>' . print_r($e, true) . '</pre>';
     }
+    echo '<pre style="background:#498248;color:white;">' . print_r($metadonnee, true) . '</pre>';
+    $ApidaeMetadata->stop('get ' . $mdCpt++);
+}
 
-    echo '<h1>Valeur origine</h1>' ;
-    showMd() ;
+echo '<h1>Valeur origine</h1>';
+showMd();
 
-    $ApidaeMetadata->start('delete') ;
-    echo '<h1>Delete...</h1>' ;
-    $ApidaeMetadata->delete($offre,$noeud) ;
-    showMd() ;
-    $ApidaeMetadata->stop('delete') ;
+$ApidaeMetadata->start('delete');
+echo '<h1>Delete...</h1>';
+$ApidaeMetadata->delete($offre, $noeud);
+showMd();
+$ApidaeMetadata->stop('delete');
 
-    // Insert single
-    $ApidaeMetadata->start('insert single') ;
-    echo '<hr /><h1>Insert general (single)</h1>' ;
-    $general = <<<'JSON'
+// Insert single
+$ApidaeMetadata->start('insert single');
+echo '<hr /><h1>Insert general (single)</h1>';
+$general = <<<'JSON'
         {"infoGenerale":"Ceci est une information a portée générale"}
     JSON;
-    
-    try {
-        $res = $ApidaeMetadata->put($offre,$noeud,http_build_query(Array('general'=>$general))) ;
-    } catch (\Exception $e) {
-        echo '<h1>'.__FILE__.':'.__LINE__.'</h1>' ;
-        echo '<pre>' ; print_r($e) ; echo '</pre>' ;
-        return ;
-    }
-    $ApidaeMetadata->stop('insert single') ;
 
-    showMd() ;
+try {
+    $res = $ApidaeMetadata->put($offre, $noeud, http_build_query(array('general' => $general)));
+} catch (\Exception $e) {
+    echo '<h1>' . __FILE__ . ':' . __LINE__ . '</h1>';
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+    return;
+}
+$ApidaeMetadata->stop('insert single');
 
-    // Insert single
-    $ApidaeMetadata->start('insert single membre') ;
-    echo '<hr /><h1>Insert membre (single)</h1>' ;
-    $membre = <<<'JSON'
+showMd();
+
+// Insert single
+$ApidaeMetadata->start('insert single membre');
+echo '<hr /><h1>Insert membre (single)</h1>';
+$membre = <<<'JSON'
         {"infoGenerale":"Info Single membre 1157"}
     JSON;
-    
-    try {
-        $res = $ApidaeMetadata->put($offre,$noeud,http_build_query(Array('membres.membre_1157'=>$membre))) ;
-    } catch (\Exception $e) {
-        echo '<h1>'.__FILE__.':'.__LINE__.'</h1>' ;
-        echo '<pre>' ; print_r($e) ; echo '</pre>' ;
-        return ;
-    }
-    $ApidaeMetadata->stop('insert single membre') ;
 
-    showMd() ;
+try {
+    $res = $ApidaeMetadata->put($offre, $noeud, http_build_query(array('membres.membre_1157' => $membre)));
+} catch (\Exception $e) {
+    echo '<h1>' . __FILE__ . ':' . __LINE__ . '</h1>';
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+    return;
+}
+$ApidaeMetadata->stop('insert single membre');
+
+showMd();
 
 
-    $ApidaeMetadata->start('insert membres') ;
-    // Insert type membres
-    echo '<hr /><h1>Insert membres</h1>' ;
-    $membres = <<<'JSON'
+$ApidaeMetadata->start('insert membres');
+// Insert type membres
+echo '<hr /><h1>Insert membres</h1>';
+$membres = <<<'JSON'
     [
         {
             "targetId":21,
@@ -84,22 +93,24 @@
     ]
     JSON;
 
-    try {
-        $res = $ApidaeMetadata->put($offre,$noeud,http_build_query(Array('membres'=>$membres))) ;
-    } catch (\Exception $e) {
-        echo '<h2>'.__FILE__.':'.__LINE__.'</h2>' ;
-        echo '<pre>' ; print_r($e) ; echo '</pre>' ;
-        return ;
-    }
-    $ApidaeMetadata->stop('insert membres') ;
+try {
+    $res = $ApidaeMetadata->put($offre, $noeud, http_build_query(array('membres' => $membres)));
+} catch (\Exception $e) {
+    echo '<h2>' . __FILE__ . ':' . __LINE__ . '</h2>';
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+    return;
+}
+$ApidaeMetadata->stop('insert membres');
 
-    showMd() ;
+showMd();
 
-    // Insert type projet
+// Insert type projet
 
-    $ApidaeMetadata->start('insert projet') ;
-    echo '<hr /><h1>Insert projet</h1>' ;
-    $projets = <<<'JSON'
+$ApidaeMetadata->start('insert projet');
+echo '<hr /><h1>Insert projet</h1>';
+$projets = <<<'JSON'
     [
         {
             "targetId":1337,
@@ -112,23 +123,25 @@
     ]
     JSON;
 
-    try {
-        $res = $ApidaeMetadata->put($offre,$noeud,http_build_query(Array('projets'=>$projets))) ;
-    } catch (\Exception $e) {
-        echo '<h2>'.__FILE__.':'.__LINE__.'</h2>' ;
-        echo '<pre>' ; print_r($e) ; echo '</pre>' ;
-        return ;
-    }
+try {
+    $res = $ApidaeMetadata->put($offre, $noeud, http_build_query(array('projets' => $projets)));
+} catch (\Exception $e) {
+    echo '<h2>' . __FILE__ . ':' . __LINE__ . '</h2>';
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+    return;
+}
 
-    $ApidaeMetadata->stop('insert projet') ;
+$ApidaeMetadata->stop('insert projet');
 
-    showMd() ;
+showMd();
 
-    // Insert multiple
+// Insert multiple
 
-    $ApidaeMetadata->start('insert multiple') ;
-    echo '<hr /><h1>Insert multiple</h1>' ;
-    $node = <<<'JSON'
+$ApidaeMetadata->start('insert multiple');
+echo '<hr /><h1>Insert multiple</h1>';
+$node = <<<'JSON'
     {
         "general" : "{\"info\":\"général par insertion multiple\"}",
         "membres": [
@@ -154,22 +167,24 @@
     }
     JSON;
 
-    echo '<pre><code>'.$node.'</code></pre>' ;
+echo '<pre><code>' . $node . '</code></pre>';
 
-    try {
-        $res = $ApidaeMetadata->put($offre,$noeud,http_build_query(Array('node'=>$node))) ;
-    } catch (\Exception $e) {
-        echo '<h2>'.__FILE__.':'.__LINE__.'</h2>' ;
-        echo '<pre>' ; print_r($e) ; echo '</pre>' ;
-        return ;
-    }
+try {
+    $res = $ApidaeMetadata->put($offre, $noeud, http_build_query(array('node' => $node)));
+} catch (\Exception $e) {
+    echo '<h2>' . __FILE__ . ':' . __LINE__ . '</h2>';
+    echo '<pre>';
+    print_r($e);
+    echo '</pre>';
+    return;
+}
 
-    $ApidaeMetadata->stop('insert multiple') ;
+$ApidaeMetadata->stop('insert multiple');
 
-    showMd() ;
+showMd();
 
-    $ApidaeMetadata->stop(__FILE__) ;
-    $ApidaeMetadata->timer() ;
+$ApidaeMetadata->stop(__FILE__);
+$ApidaeMetadata->timer();
 
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
--- src/ApidaeMetadata.php
+++ src/ApidaeMetadata.php
@@ -1,76 +1,81 @@
 <?php
 
-	namespace PierreGranger ;
-	use PierreGranger\ApidaeCore ;
+namespace PierreGranger;
+
+use PierreGranger\ApidaeCore;
+use PierreGranger\ApidaeException;
+
 /**
-*
-* @author  Pierre Granger <pierre.granger@apidae-tourisme.com>
-*
-*/
+ *
+ * @author  Pierre Granger <pierre.granger@apidae-tourisme.com>
+ *
+ */
 
-	class ApidaeMetadata extends ApidaeCore {
+class ApidaeMetadata extends ApidaeCore
+{
 
-		/**
-		 * clientId par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
-		 * Format GUID. Ex : b2d52993-9712-4346-8108-d67dfc51dea0
-		 * @var	string
-		 */
-		protected $clientId ;
-		/** 
-		 * secret par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
-		 * 15 caractères alphanumériques. Ex : aOzHUt3H227zivv
-		 * @var	string
-		 */
-		protected $secret ;
+	/**
+	 * clientId par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
+	 * Format GUID. Ex : b2d52993-9712-4346-8108-d67dfc51dea0
+	 * @var	string
+	 */
+	protected $clientId;
+	/** 
+	 * secret par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
+	 * 15 caractères alphanumériques. Ex : aOzHUt3H227zivv
+	 * @var	string
+	 */
+	protected $secret;
 
-		protected const targets = Array('general','membre','projet') ;
+	protected const targets = ['general', 'membre', 'projet'];
 
-		public function __construct(array $params=null) {
+	public function __construct(array $params = null)
+	{
 
-			parent::__construct($params) ;
+		parent::__construct($params);
 
-			if ( ! is_array($params) ) $params = Array() ;
-			
-			if ( isset($params['clientId']) ) $this->clientId = $params['clientId'] ;
-			if ( isset($params['secret']) ) $this->secret = $params['secret'] ;
+		if (!is_array($params)) $params = [];
 
-		}
+		if (isset($params['clientId'])) $this->clientId = $params['clientId'];
+		if (isset($params['secret'])) $this->secret = $params['secret'];
+	}
 
-		public function get($id,$noeud,$params=null) {
-			
-			if ( ! is_array($params) ) $params  = Array() ;
+	public function get($id, $noeud, $params = null)
+	{
 
-			$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId ;
-			$secret = isset($params['secret']) ? $params['secret'] : $this->secret ;
+		if (!is_array($params)) $params  = [];
 
-			if ( $clientId == null ) throw new ApidaeException('clientId non renseigné',ApidaeException::MISSING_PARAMETER) ;
-			if ( $secret == null ) throw new ApidaeException('secret non renseigné',ApidaeException::MISSING_PARAMETER) ;
+		$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+		$secret = isset($params['secret']) ? $params['secret'] : $this->secret;
 
-			$access_token = $this->gimme_token($clientId,$secret) ;
-			if ( ! $access_token ) throw new ApidaeException(__LINE__.'Impossible de récupérer le token d\'écriture') ;
-				
-			$result = $this->request('/api/v002/metadata/'.$id.'/'.$noeud.'/',Array(
-				'token' => $access_token,
-				'CUSTOMREQUEST' => 'GET'
-			)) ;
-			
-			if ( $result['code'] != 200 ) throw new ApidaeException('Retour http incorrect',ApidaeException::INVALID_HTTPCODE,Array(
-				'debug' => $this->debug,
-				'result' => $result
-			)) ;
-			return $result['body'] ;
-		}
+		if ($clientId == null) throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+		if ($secret == null) throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
 
-		/**
-		 * @param	int	$id	Identiant de l'offre. Ex : 4683815
-		 * @param	string	$noeud	Nom du noeud. Ex : open-system
-		 * @param	string	$data	Métadonnées, au format récupéré dans un objet en lecture. Ex : {}
-		 * @param	null|array	$params	Paramètres additionnels
-		 * 						$params['clientId']
-		 * 						$params['secret']
-		 */
+		$access_token = $this->gimme_token($clientId, $secret);
+		if (!$access_token) throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
 
-		/*
+		$result = $this->request('/api/v002/metadata/' . $id . '/' . $noeud . '/', [
+			'token' => $access_token,
+			'CUSTOMREQUEST' => 'GET'
+		]);
+
+		if ($result['code'] != 200) throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+			'debug' => $this->debug,
+			'result' => $result
+		]);
+		return $result['body'];
+	}
+
+	/**
+	 * @param	int	$id	Identiant de l'offre. Ex : 4683815
+	 * @param	string	$noeud	Nom du noeud. Ex : open-system
+	 * @param	string	$data	Métadonnées, au format récupéré dans un objet en lecture. Ex : {}
+	 * @param	null|array	$params	Paramètres additionnels
+	 * 						$params['clientId']
+	 * 						$params['secret']
+	 */
+
+	/*
 			Exemple de métadonnée
 			{
 				"widget": {
@@ -89,68 +94,67 @@
 			}
 		*/
 
-		public function put($id,$noeud,$postfields,$params=null) {
+	public function put($id, $noeud, $postfields, $params = null)
+	{
 
-			if ( ! is_array($params) ) $params  = Array() ;
-			
-			$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId ;
-			$secret = isset($params['secret']) ? $params['secret'] : $this->secret ;
+		if (!is_array($params)) $params  = [];
 
-			if ( $clientId == null ) throw new ApidaeException('clientId non renseigné',ApidaeException::MISSING_PARAMETER) ;
-			if ( $secret == null ) throw new ApidaeException('secret non renseigné',ApidaeException::MISSING_PARAMETER) ;
+		$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+		$secret = isset($params['secret']) ? $params['secret'] : $this->secret;
 
-			$access_token = $this->gimme_token($clientId,$secret) ;
-			if ( ! $access_token ) throw new ApidaeException(__LINE__.'Impossible de récupérer le token d\'écriture') ;
+		if ($clientId == null) throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+		if ($secret == null) throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
 
-			$result = $this->request('/api/v002/metadata/'.$id.'/'.$noeud.'/',Array(
-				'token' => $access_token,
-				'POSTFIELDS' => $postfields,
-				'CUSTOMREQUEST' => 'PUT'
-			)) ;
-			
-			if ( $result['code'] != 200 ) throw new ApidaeException('Retour http incorrect',ApidaeException::INVALID_HTTPCODE,Array(
-				'debug' => $this->debug,
-				'result' => $result
-			)) ;
-			
-			return true ;
-		}
+		$access_token = $this->gimme_token($clientId, $secret);
+		if (!$access_token) throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
 
-		public function delete($id,$noeud,$params=null) {
-			if ( ! is_array($params) ) $params  = Array() ;
-			
-			$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId ;
-			$secret = isset($params['secret']) ? $params['secret'] : $this->secret ;
+		$result = $this->request('/api/v002/metadata/' . $id . '/' . $noeud . '/', [
+			'token' => $access_token,
+			'POSTFIELDS' => $postfields,
+			'CUSTOMREQUEST' => 'PUT'
+		]);
 
-			if ( $clientId == null ) throw new ApidaeException('clientId non renseigné',ApidaeException::MISSING_PARAMETER) ;
-			if ( $secret == null ) throw new ApidaeException('secret non renseigné',ApidaeException::MISSING_PARAMETER) ;
+		if ($result['code'] != 200) throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+			'debug' => $this->debug,
+			'result' => $result
+		]);
 
-			$access_token = $this->gimme_token($clientId,$secret) ;
-			if ( ! $access_token ) throw new ApidaeException(__LINE__.'Impossible de récupérer le token d\'écriture') ;
-
-			$curl_url = '/api/v002/metadata/'.$id.'/'.$noeud.'/' ;
-			if ( isset($params['targetType']) )
-			{
-				if ( ! in_array($params['targetType'],self::$targets) ) throw new \Exception('targetType not in '.implode(', ',self::targets)) ;
-				else $curl_url .= $params['targetType'].'/' ;
-			}
-			if ( isset($params['targetId']) )
-			{
-				if ( ! isset($params['targetType']) ) throw new \Exception('targetId defined : targetType required') ;
-				if ( ! preg_match('#^[0-9]+$#',$params['targetId']) ) throw new \Exception('targetId is not a number') ;
-				else $curl_url .= (int)$params['targetId'].'/' ;
-			}
-
-			$result = $this->request($curl_url,Array(
-				'token' => $access_token,
-				'CUSTOMREQUEST' => 'DELETE'
-			));
-			
-			if ( $result['code'] != 200 ) throw new ApidaeException('Retour http incorrect',ApidaeException::INVALID_HTTPCODE,Array(
-				'debug' => $this->debug,
-				'result' => $result	
-			)) ;
-			return true ;
-		}
-
+		return true;
 	}
+
+	public function delete($id, $noeud, $params = null)
+	{
+		if (!is_array($params)) $params  = [];
+
+		$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+		$secret = isset($params['secret']) ? $params['secret'] : $this->secret;
+
+		if ($clientId == null) throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+		if ($secret == null) throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+
+		$access_token = $this->gimme_token($clientId, $secret);
+		if (!$access_token) throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+
+		$curl_url = '/api/v002/metadata/' . $id . '/' . $noeud . '/';
+		if (isset($params['targetType'])) {
+			if (!in_array($params['targetType'], self::$targets)) throw new \Exception('targetType not in ' . implode(', ', self::targets));
+			else $curl_url .= $params['targetType'] . '/';
+		}
+		if (isset($params['targetId'])) {
+			if (!isset($params['targetType'])) throw new \Exception('targetId defined : targetType required');
+			if (!preg_match('#^[0-9]+$#', $params['targetId'])) throw new \Exception('targetId is not a number');
+			else $curl_url .= (int)$params['targetId'] . '/';
+		}
+
+		$result = $this->request($curl_url, [
+			'token' => $access_token,
+			'CUSTOMREQUEST' => 'DELETE'
+		]);
+
+		if ($result['code'] != 200) throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+			'debug' => $this->debug,
+			'result' => $result
+		]);
+		return true;
+	}
+}
