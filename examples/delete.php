aaac39766103d0de95dd8914139dce102c5207e1
Pierre Granger <pierre.granger@apidae-tourisme.com>
2022-11-08 16:46:51 +0100

    Minor examples changes

--- examples/delete.php
+++ examples/delete.php
@@ -1,24 +1,30 @@
 <?php
 
-    ini_set('display_errors',1) ;
-    error_reporting(E_ALL) ;
+ini_set('display_errors', 1) ;
+error_reporting(E_ALL) ;
 
-    require_once(realpath(dirname(__FILE__)).'/../vendor/autoload.php') ;
-    require_once(realpath(dirname(__FILE__)).'/../config.inc.php') ;
+require_once(realpath(dirname(__FILE__)).'/../vendor/autoload.php') ;
+require_once(realpath(dirname(__FILE__)).'/../config.inc.php') ;
 
-    $ApidaeMetadata = new \PierreGranger\ApidaeMetadata(array_merge($configApidaeMetadata,Array('debug'=>true))) ;
-    
-    echo '<h1>Avant...</h1>' ;
-    try {
-        echo '<pre>'.print_r($ApidaeMetadata->get(4683815,'test-pg'),true).'</pre>' ;
-    } catch ( Exception $e ) { echo $e ; }
+$ApidaeMetadata = new \PierreGranger\ApidaeMetadata(array_merge($configApidaeMetadata, array('debug'=>true))) ;
 
-    echo '<h1>Delete...</h1>' ;
-    try {
-        $retour = $ApidaeMetadata->delete($offre,$noeud) ;
-    } catch ( Exception $e ) { echo $e ; }
+echo '<h1>Avant...</h1>' ;
+try {
+    echo '<pre>'.print_r($ApidaeMetadata->get(4683815, 'test-pg'), true).'</pre>' ;
+} catch (Exception $e) {
+    var_dump($e) ;
+}
 
-    echo '<h1>Après...</h1>' ;
-    try {
-        echo '<pre>'.print_r($ApidaeMetadata->get(4683815,'test-pg'),true).'</pre>' ;
-    } catch ( Exception $e ) { echo $e ; }
+echo '<h1>Delete...</h1>' ;
+try {
+    $retour = $ApidaeMetadata->delete($offre, $noeud) ;
+} catch (Exception $e) {
+    var_dump($e) ;
+}
+
+echo '<h1>Après...</h1>' ;
+try {
+    echo '<pre>'.print_r($ApidaeMetadata->get(4683815, 'test-pg'), true).'</pre>' ;
+} catch (Exception $e) {
+    var_dump($e) ;
+}
--- examples/get.php
+++ examples/get.php
@@ -1,19 +1,21 @@
 <?php
 
-    ini_set('display_errors',1) ;
-    error_reporting(E_ALL) ;
+ini_set('display_errors', 1) ;
+error_reporting(E_ALL) ;
 
-    require_once(realpath(dirname(__FILE__)).'/../vendor/autoload.php') ;
-    require_once(realpath(dirname(__FILE__)).'/../config.inc.php') ;
+require_once(realpath(dirname(__FILE__)).'/../vendor/autoload.php') ;
+require_once(realpath(dirname(__FILE__)).'/../config.inc.php') ;
 
-    $ApidaeMetadata = new \PierreGranger\ApidaeMetadata(array_merge($configApidaeMetadata,Array('debug'=>true,'timer'=>true))) ;
+$ApidaeMetadata = new \PierreGranger\ApidaeMetadata(array_merge($configApidaeMetadata, array('debug'=>true,'timer'=>true))) ;
 
-    $ApidaeMetadata->start(__FILE__) ;
-    try {
-        $metadonnee = $ApidaeMetadata->get(4683815,'test-pg') ;
-        echo '<pre>'.print_r($metadonnee,true).'</pre>' ;
-    } catch ( Exception $e ) { echo $e ; }
-    
-    $ApidaeMetadata->stop(__FILE__) ;
+$ApidaeMetadata->start(__FILE__) ;
+try {
+    $metadonnee = $ApidaeMetadata->get(4683815, 'test-pg') ;
+    echo '<pre>'.print_r($metadonnee, true).'</pre>' ;
+} catch (Exception $e) {
+    echo $e ;
+}
 
-    $ApidaeMetadata->timer() ;
+$ApidaeMetadata->stop(__FILE__) ;
+
+$ApidaeMetadata->timer() ;
