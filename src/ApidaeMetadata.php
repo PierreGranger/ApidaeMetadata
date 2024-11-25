53db8126b576f93daafa09aa47454295160130a8
Pierre Granger <pierre.granger@apidae-tourisme.com>
2024-11-25 15:56:07 +0100

    +headers

--- src/ApidaeMetadata.php
+++ src/ApidaeMetadata.php
@@ -13,148 +13,237 @@ use PierreGranger\ApidaeException;
 
 class ApidaeMetadata extends ApidaeCore
 {
+    /**
+     * clientId par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
+     * Format GUID. Ex : b2d52993-9712-4346-8108-d67dfc51dea0
+     * @var	string
+     */
+    protected $clientId;
+    /**
+     * secret par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
+     * 15 caractères alphanumériques. Ex : aOzHUt3H227zivv
+     * @var	string
+     */
+    protected $secret;
 
-	/**
-	 * clientId par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
-	 * Format GUID. Ex : b2d52993-9712-4346-8108-d67dfc51dea0
-	 * @var	string
-	 */
-	protected $clientId;
-	/** 
-	 * secret par défaut, fourni lors de l'implémentation de l'objet (new ...). S'il n'est pas renseigné il pourra être passé en paramètre des classes qui en ont besoin
-	 * 15 caractères alphanumériques. Ex : aOzHUt3H227zivv
-	 * @var	string
-	 */
-	protected $secret;
+    protected const TARGETS = ['general', 'membre', 'projet'];
 
-	protected const targets = ['general', 'membre', 'projet'];
+    public function __construct(array $params = null)
+    {
+        parent::__construct($params);
 
-	public function __construct(array $params = null)
-	{
+        if (!is_array($params)) {
+            $params = [];
+        }
 
-		parent::__construct($params);
+        if (isset($params['clientId'])) {
+            $this->clientId = $params['clientId'];
+        }
+        if (isset($params['secret'])) {
+            $this->secret = $params['secret'];
+        }
+    }
 
-		if (!is_array($params)) $params = [];
+    public function get($id, $noeud, $params = null)
+    {
+        if (!is_array($params)) {
+            $params  = [];
+        }
 
-		if (isset($params['clientId'])) $this->clientId = $params['clientId'];
-		if (isset($params['secret'])) $this->secret = $params['secret'];
-	}
+        $clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+        $secret = isset($params['secret']) ? $params['secret'] : $this->secret;
 
-	public function get($id, $noeud, $params = null)
-	{
+        if ($clientId == null) {
+            throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
+        if ($secret == null) {
+            throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
 
-		if (!is_array($params)) $params  = [];
+        $access_token = $this->gimme_token($clientId, $secret);
+        if (!$access_token) {
+            throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        }
 
-		$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
-		$secret = isset($params['secret']) ? $params['secret'] : $this->secret;
+        $requestParams = [
+            'token' => $access_token,
+            'CUSTOMREQUEST' => 'GET'
+        ] ;
+        if ( isset($params['header']) ) $requestParams['header'] = $params['header'] ;
 
-		if ($clientId == null) throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
-		if ($secret == null) throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        $result = $this->request('/api/v002/metadata/' . $id . '/' . $noeud . '/', $requestParams);
 
-		$access_token = $this->gimme_token($clientId, $secret);
-		if (!$access_token) throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        if ($result['code'] != 200) {
+            throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+                'debug' => $this->debug,
+                'result' => $result
+            ]);
+        }
+        return $result['body'];
+    }
 
-		$result = $this->request('/api/v002/metadata/' . $id . '/' . $noeud . '/', [
-			'token' => $access_token,
-			'CUSTOMREQUEST' => 'GET'
-		]);
+    /**
+     * @param	int	$id	Identiant de l'offre. Ex : 4683815
+     * @param	string	$noeud	Nom du noeud. Ex : open-system
+     * @param	string	$data	Métadonnées, au format récupéré dans un objet en lecture. Ex : {}
+     * @param	null|array	$params	Paramètres additionnels
+     * 						$params['clientId']
+     * 						$params['secret']
+     */
 
-		if ($result['code'] != 200) throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
-			'debug' => $this->debug,
-			'result' => $result
-		]);
-		return $result['body'];
-	}
+    /*
+            Exemple de métadonnée
+            {
+                "widget": {
+                    "v": "1.0",
+                    "uis": [
+                        {
+                            "ui": "CA-ITEAG-29234-G4797",
+                            "metier": "hebergement"
+                        }
+                    ],
+                    "integration": {
+                        "id": 370,
+                        "idPanier": "FBgUFBQ"
+                    }
+                }
+            }
+        */
 
-	/**
-	 * @param	int	$id	Identiant de l'offre. Ex : 4683815
-	 * @param	string	$noeud	Nom du noeud. Ex : open-system
-	 * @param	string	$data	Métadonnées, au format récupéré dans un objet en lecture. Ex : {}
-	 * @param	null|array	$params	Paramètres additionnels
-	 * 						$params['clientId']
-	 * 						$params['secret']
-	 */
+    public function putMultiple($query, array $params = null)
+    {
+        if (!is_array($params)) {
+            $params  = [];
+        }
 
-	/*
-			Exemple de métadonnée
-			{
-				"widget": {
-					"v": "1.0",
-					"uis": [
-						{
-							"ui": "CA-ITEAG-29234-G4797",
-							"metier": "hebergement"
-						}
-					],
-					"integration": {
-						"id": 370,
-						"idPanier": "FBgUFBQ"
-					}
-				}
-			}
-		*/
+        $clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+        $secret = isset($params['secret']) ? $params['secret'] : $this->secret;
 
-	public function put($id, $noeud, $postfields, $params = null)
-	{
+        if ($clientId == null) {
+            throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
+        if ($secret == null) {
+            throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
 
-		if (!is_array($params)) $params  = [];
+        $access_token = $this->gimme_token($clientId, $secret);
+        if (!$access_token) {
+            throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        }
 
-		$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
-		$secret = isset($params['secret']) ? $params['secret'] : $this->secret;
+        $requestParams = [
+            'token' => $access_token,
+            'POSTFIELDS' => ['query' => json_encode($query)],
+            'CUSTOMREQUEST' => 'PUT'
+        ] ;
+        if ( isset($params['header']) ) $requestParams['header'] = $params['header'] ;
 
-		if ($clientId == null) throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
-		if ($secret == null) throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        $result = $this->request('/api/v002/metadata/', $requestParams);
 
-		$access_token = $this->gimme_token($clientId, $secret);
-		if (!$access_token) throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        if ($result['code'] != 200) {
+            throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+                'debug' => $this->debug,
+                'result' => $result
+            ]);
+        }
 
-		$result = $this->request('/api/v002/metadata/' . $id . '/' . $noeud . '/', [
-			'token' => $access_token,
-			'POSTFIELDS' => $postfields,
-			'CUSTOMREQUEST' => 'PUT'
-		]);
+        return true;
+    }
 
-		if ($result['code'] != 200) throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
-			'debug' => $this->debug,
-			'result' => $result
-		]);
+    public function put(int $id, string $noeud, array|string $postfields, array $params = null)
+    {
+        if (!is_array($params)) {
+            $params  = [];
+        }
 
-		return true;
-	}
+        $clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+        $secret = isset($params['secret']) ? $params['secret'] : $this->secret;
 
-	public function delete($id, $noeud, $params = null)
-	{
-		if (!is_array($params)) $params  = [];
+        if ($clientId == null) {
+            throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
+        if ($secret == null) {
+            throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
 
-		$clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
-		$secret = isset($params['secret']) ? $params['secret'] : $this->secret;
+        $access_token = $this->gimme_token($clientId, $secret);
+        if (!$access_token) {
+            throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        }
 
-		if ($clientId == null) throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
-		if ($secret == null) throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        $requestParams = [
+            'token' => $access_token,
+            'POSTFIELDS' => $postfields,
+            'CUSTOMREQUEST' => 'PUT'
+        ] ;
+        if ( isset($params['header']) ) $requestParams['header'] = $params['header'] ;
 
-		$access_token = $this->gimme_token($clientId, $secret);
-		if (!$access_token) throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        $result = $this->request('/api/v002/metadata/' . $id . '/' . $noeud . '/', $requestParams);
 
-		$curl_url = '/api/v002/metadata/' . $id . '/' . $noeud . '/';
-		if (isset($params['targetType'])) {
-			if (!in_array($params['targetType'], self::$targets)) throw new \Exception('targetType not in ' . implode(', ', self::targets));
-			else $curl_url .= $params['targetType'] . '/';
-		}
-		if (isset($params['targetId'])) {
-			if (!isset($params['targetType'])) throw new \Exception('targetId defined : targetType required');
-			if (!preg_match('#^[0-9]+$#', $params['targetId'])) throw new \Exception('targetId is not a number');
-			else $curl_url .= (int)$params['targetId'] . '/';
-		}
+        if ($result['code'] != 200) {
+            throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+                'debug' => $this->debug,
+                'result' => $result
+            ]);
+        }
 
-		$result = $this->request($curl_url, [
-			'token' => $access_token,
-			'CUSTOMREQUEST' => 'DELETE'
-		]);
+        return true;
+    }
 
-		if ($result['code'] != 200) throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
-			'debug' => $this->debug,
-			'result' => $result
-		]);
-		return true;
-	}
+    public function delete($id, $noeud, $params = null)
+    {
+        if (!is_array($params)) {
+            $params  = [];
+        }
+
+        $clientId = isset($params['clientId']) ? $params['clientId'] : $this->clientId;
+        $secret = isset($params['secret']) ? $params['secret'] : $this->secret;
+
+        if ($clientId == null) {
+            throw new ApidaeException('clientId non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
+        if ($secret == null) {
+            throw new ApidaeException('secret non renseigné', ApidaeException::MISSING_PARAMETER);
+        }
+
+        $access_token = $this->gimme_token($clientId, $secret);
+        if (!$access_token) {
+            throw new ApidaeException(__LINE__ . 'Impossible de récupérer le token d\'écriture');
+        }
+
+        $curl_url = '/api/v002/metadata/' . $id . '/' . $noeud . '/';
+        if (isset($params['targetType'])) {
+            if (!in_array($params['targetType'], self::TARGETS)) {
+                throw new \Exception('targetType not in ' . implode(', ', self::TARGETS));
+            } else {
+                $curl_url .= $params['targetType'] . '/';
+            }
+        }
+        if (isset($params['targetId'])) {
+            if (!isset($params['targetType'])) {
+                throw new \Exception('targetId defined : targetType required');
+            }
+            if (!preg_match('#^[0-9]+$#', $params['targetId'])) {
+                throw new \Exception('targetId is not a number');
+            } else {
+                $curl_url .= (int)$params['targetId'] . '/';
+            }
+        }
+
+        $requestParams = [
+            'token' => $access_token,
+            'CUSTOMREQUEST' => 'DELETE'
+        ] ;
+        if ( isset($params['header']) ) $requestParams['header'] = $params['header'] ;
+
+        $result = $this->request($curl_url, $requestParams);
+
+        if ($result['code'] != 200) {
+            throw new ApidaeException('Retour http incorrect', ApidaeException::INVALID_HTTPCODE, [
+                'debug' => $this->debug,
+                'result' => $result
+            ]);
+        }
+        return true;
+    }
 }
